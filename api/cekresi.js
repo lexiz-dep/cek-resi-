630907a88e0a69ec5586292c2370382f2cc6b29e
lexiz-dep <leximuze@gmail.com>
2025-07-14 14:39:06 +0700

    Add files via upload

--- api/cekresi.js
+++ api/cekresi.js
@@ -10,3 +10,5 @@ export default async function handler(req, res) {
   const result = await cekresi(noresi, ekspedisi);
   res.status(200).json(result);
 }
+
+//   Created By Kai Contact https://wa.me/6282126446272 Pake Sc Error? Join Saja Disini dan report menggunakan disqus yang disediakan di desk > https://whatsapp.com/channel/0029VanrndJICVfcrjFr3x2R  > https://whatsapp.com/channel/0029VanrndJICVfcrjFr3x2R > https://whatsapp.com/channel/0029VanrndJICVfcrjFr3x2R Mau Jasa bikin web? porto? Chat Saja   Menerima tapi html css JavaScript aja soalnya yang lain malas //*
